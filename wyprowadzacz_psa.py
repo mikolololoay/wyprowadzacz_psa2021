a12319d88f0a8d6b0ec0a5e0e6bd2dcba1c6e171
szynol <40857143+szynol@users.noreply.github.com>
2021-03-31 21:07:37 +0200

    rozgrzebana funkcja wyprowadzania psa

--- wyprowadzacz_psa.py
+++ wyprowadzacz_psa.py
@@ -1,9 +1,10 @@
-def wyprowadz_psa(imie, wiek_psa)
-    if wiek_psa >= 7: # w latach
-        czas_spaceru = 30 # w minutach
-    else:
-        czas_spaceru = 60
-    print(f'{imie}, do nogi!')
+def wyprowadz_psa(imie_psa, wiek_psa, pogoda)
+    czas_spaceru = 100 / wiek_psa
+    if czas_spaceru < 15:
+        czas_spaceru = 15
+    if pogoda == 'deszcz' or pogoda == 'burza':
+        czas_spaceru -= 10
+    print(f'{imie_psa}, do nogi!')
     return czas_spaceru
     
 wyprowadz_psa('Azor', 9)
